475ef7b5c74d6eef61d08e61d69358ff0f12b59c
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-11-06 16:44:50 -0300

    Formatting the output of the API based on the event code and type

--- internal/pkg/constants.go
+++ internal/pkg/constants.go
@@ -2,3 +2,22 @@ package pkg
 
 // CorreiosBaseURL correios API base URL
 const CorreiosBaseURL = "https://proxyapp.correios.com.br/v1/sro-rastro/%s"
+
+// IconDictionary represents a dictionary that map dto.Event Code to Icons
+var IconDictionary = map[string]string{
+	"BDE01": "🎁",
+	"BDE20": "📪",
+	"OEC01": "🙌",
+	"DO01":  "🚚",
+	"RO01":  "🚚",
+	"PO01":  "📦",
+	"PAR10": "🔎✅",
+	"PAR16": "🛬",
+	"PAR17": "💸",
+	"PAR18": "🗺",
+	"PAR21": "🔎",
+	"PAR24": "🔙",
+	"PAR26": "🙅",
+	"PAR31": "🤑",
+	"":      "🚧",
+}
